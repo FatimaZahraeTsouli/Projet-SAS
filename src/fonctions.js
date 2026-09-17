2bac1cfd5a8f4659dd9102a77b5872f6f7f3e9be
FatimaZahraeTsouli <https://github.com/FatimaZahraeTsouli/day1.git>
2026-09-17 19:30:51 +0200

    fonctions

--- src/fonctions.js
+++ src/fonctions.js
@@ -1,80 +1,97 @@
-//  ************fonction normalisernom: done*****************
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+import { apprenants } from './data.js'
 
-// import { apprenants } from './data.js'
-// export function normaliserNom(apprenant){
-//   return apprenants.map(function(apprenant){
-//     return apprenant.nom = apprenant.nomComplet.trim().toLowerCase()
-//     })
-// }
+
+//  ************1.fonction normalisernom: done*****************
+//  let nom = prompt("entrez le nom : ")
+// export function normaliserNom(){
+//     let nom1 = nom.trim().toLowerCase()
+//     return `votre nom est : ${nom1}`
+//     }
 // normaliserNom()
 
 
-// *****Fonction validerResultat: undone*******
+// *****2.Fonction validerResultat: done*******
 
-import { apprenants } from './data.js'
-export function validerResultat() {
-    return apprenants.map(function (apprenants) {
-        return apprenants.resultats.map(function(resultats){
-        if (resultats.jour >= 0 && resultats.jour <= 7) {
-            return true
-        } if (resultats.totalExercices == 20) {
-            return true
-        }
-        if (resultats.exercicesTermines < resultats.totalExercices && resultats.exercicesTermines >=0 ) {
-            const totalExercices = 20
-            return true
-        }
-        if (typeof resultats.challengeTermine == "boolean") {
-            return true
-        }
-        return false
-    
-    })})}
-console.log(validerResultat())
+// export function validerResultat() {
+//     let resultats ={
+//         jour: parseInt(prompt("entrez jour : "),10),
+//         exercicesTermines: parseInt(prompt("entrez nombre d'exercices terminées :"),10),
+//         totalExercices: parseInt(prompt("entrez total d'exercices : "),10),
+//         challengeTermine :prompt("avez vous terminer votre challenge? (oui/non) :")
+//     }
+//     let challenge = resultats.challengeTermine.toLowerCase()
+//         if (resultats.jour < 1 && resultats.jour > 7) {
+//             return false
+//         } if (resultats.totalExercices !== 20) {
+//             return false
+//         }
+//         if (resultats.exercicesTermines > resultats.totalExercices && resultats.exercicesTermines <0 ) {
+//             const totalExercices = 20
+//             return false
+//         }
+//         if (challenge !== "oui" && challenge !== "non") {
+//             console.log("re-entrez 'true' ou 'false'")
+//             return false
+//         }
+//         return true
+//     }
+// console.log(validerResultat())
 
 
 
 
-// *****************fonction ajouterapprenant: done*****************
+// *****************3.fonction ajouterapprenant: done*****************
 
-//   import { apprenants } from './data.js'
-// export function ajouterApprenant(nouveauApprenant){
-//     for (let i=0; i<apprenants.length; i++){
-//     if(apprenants[i].id === nouveauApprenant.id){
-//         return("apprenant deja existe")
-//     }}
-//         apprenants.push(nouveauApprenant)
-//         return ("ajouter avec succées")
+  
+
+// export function ajouterApprenant(donnees){
+//     const nextid = apprenants.length >0 ? Math.max(...apprenants.map(nouveauApprenant=> nouveauApprenant.id))+1 :1
+//     const nomComplet= prompt("entrez votre nomComplet : ")
+//     const ville = prompt("entrez votre ville: ")
+//     const id = nextid
+//     const nouveauApprenant ={id , nomComplet, ville}
+//     apprenants.push(nouveauApprenant)
 // }
-//(ajouterApprenant())
+// (ajouterApprenant())
+// console.log({apprenants})
 
 
 
+// *****************************4.enregistrer resultats:  undone*********************************
+// import { apprenants } from './data.js'
+// export function enregistrerResultat(apprenant, nom, jour, nouveauresultat) {
+//     for (let i=0, i< apprenants.length, i++){
+//         const nom =prompt("entrez votre nom: ")
+//         const jour = prompt("entrez le jour: ")
+//         const nouveauresultat= prompt("entrez le resultat: ")
 
 
-function enregistrerResultat() {
-
-}
-
-function rechercherApprenant() {
+//     }
 
 
-}
-rechercherApprenant()
+// }
+// ***************************5.rechercher apprenant***********************************
+// function rechercherApprenant() {
 
+
+// }
+// rechercherApprenant()
+// *****************************6.calculer progression************************************
 function calculerProgression() {
 
 }
 calculerProgression()
-
+// ******************************7.filtrer par niveau**************************************
 function filtrerParNiveau() {
 
 }
-
+// ***********************************8.trier par progression*******************************
 function trierParProgression() {
 
 }
-
+// ************************************9.afficher tableau de bord****************************
 function afficherTableauDeBord() {
 
 }
---
+++ src/tests.js
@@ -0,0 +1,11 @@
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+
+
+// import {normaliserNom} from './fonctions.js'
+// console.log(normaliserNom())
+// import { ajouterApprenant } from './fonctions.js';
+// console.log( ajouterApprenant)
+
+import { validerResultat } from './fonctions.js';
+console.log (validerResultat)
